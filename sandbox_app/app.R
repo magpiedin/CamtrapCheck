a85f3f0833f5c9a04a8f585022c18f1a8a1d77a3
magpiedin <magpiedin@gmail.com>
2024-04-07 13:18:41 -0500

    add sandbox app

--- sandbox_app/app.R
+++ sandbox_app/app.R
@@ -4,20 +4,20 @@ library("readr")
 library("ggplot2")
 library("hrbrthemes")
 library("plotly")
-# App for interactive Humboldt Tree
-library(shiny)
-library(collapsibleTree)
-# Define UI for application that draws a collapsible tree
+library("shiny")
 
+# Define UI for app
 ui <- fluidPage(
   
   titlePanel("Sandbox / Chart-o-Wild Mile plants & modules"),
   
   sidebarPanel(
-    p("A hierarchical tree of ",
-      a("FMNH RI data", href = "https://pj.fieldmuseum.org/event/1eba8f5e-c5f5-49fe-a373-773244234822"),
-      "."),
-    p("For more info, see", tags$b("sandox"),
+    p("A draft-chart of plant families & locations in the",
+    a("Urban Rivers", href = "https://www.urbanriv.org/"), 
+      "Wild Mile floating gardens."),
+    p("For more info, see", 
+      a(tags$b("sandox app"), href = "https://github.com/magpiedin/CamtrapCheck/tree/main/sandbox_app"),
+      "and associated",
       a("scripts", href = "https://github.com/magpiedin/CamtrapCheck/tree/main"),
       "and",
       a("data.", href = "https://github.com/magpiedin/CamtrapCheck/tree/main/input_data/tables"))
@@ -25,7 +25,7 @@ ui <- fluidPage(
   
   # Show a tree diagram with the selected root node
   mainPanel(
-    collapsibleTreeOutput("plot")
+    plotlyOutput("plot")
   )
 )
 
@@ -40,59 +40,31 @@ server <- function(input, output) {
     paste0("family: ", indiv_plants_spp_mods$family, "\n",
            "scientific name: ", indiv_plants_spp_mods$scientificName, "\n",
            "common name: ", indiv_plants_spp_mods$common_name)
-  
-  # Configure a tree diagram
-  output$plot <- renderCollapsibleTree({
-    
-    collapsibleTreeNetwork(
-      hc_event,
-      attribute = "siteCount",
-      fill = "Color",
-      nodeSize = "leafCount"
-    )
-  })
+
+    output$plot <- renderPlotly({
+      
+      p <- 
+        ggplot(indiv_plants_spp_mods, 
+               aes(mod_plant_x, 
+                   mod_plant_y, 
+                   fill = family,
+                   text = hover_text)) + 
+        geom_tile() + 
+        theme_ipsum() + 
+        theme(
+          panel.background = element_rect(fill = "transparent",
+                                          colour = NA_character_),
+          panel.grid.minor = element_blank(),
+          plot.background = element_rect(fill = "transparent",
+                                         colour = NA_character_)
+          # Un-comment lines below (& check for missing or extra commas above/below)
+          # panel.grid.major = element_blank(), # get rid of major grid
+          )
+      
+      ggplotly(p, tooltip="text")
+      
+    })
 }
 
 # Run the application
 shinyApp(ui = ui, server = server)
-
-
-
-p <- 
-  ggplot(indiv_plants_spp_mods, 
-         aes(mod_plant_x, 
-             mod_plant_y, 
-             fill = family,
-             text = hover_text)) + 
-  geom_tile() + 
-  theme_ipsum()
-
-# Set background to transparent
-p <- p + theme(
-  panel.background = element_rect(fill = "transparent",
-                                  colour = NA_character_), # necessary to avoid drawing panel outline
-  panel.grid.minor = element_blank(), # get rid of minor grid
-  plot.background = element_rect(fill = "transparent",
-                                 colour = NA_character_) # necessary to avoid drawing plot outline
-  # Un-comment lines below (& check for missing or extra commas above/below)
-  # panel.grid.major = element_blank(), # get rid of major grid
-  # legend.background = element_rect(fill = "transparent"),
-  # legend.box.background = element_rect(fill = "transparent"),
-  # legend.key = element_rect(fill = "transparent")
-)
-
-
-ggplotly(p, tooltip="text")
-
-# Export a transparent PNG of the chart
-ggsave(
-  plot = p,
-  filename = "transparent_chart.png",
-  bg = "transparent"
-)
-
-
-# TO DO 
-# -- Tying in / overlaying camtrap data?
-# -- e.g. maybe a way to display plant or other images on hover?
-# https://stackoverflow.com/questions/73280006/tooltip-inserting-image-and-or-ggplot-in-plotly-r
